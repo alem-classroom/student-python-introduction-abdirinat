e2ea9b75636b3bd05bad041aeea50eae53a47e68
abdirinat <72246332+abdirinat@users.noreply.github.com>
2021-04-13 16:08:50 +0600

    Update conditional.py

--- if-else/conditional.py
+++ if-else/conditional.py
@@ -7,21 +7,21 @@ def is_positive(num):
         return False
 
 def is_even(num):
-    if num % 0 == 0:
+    if num % 2 == 0:
         return True
     else:
         return False
 
 
 def is_positive_and_even(num):
-    if num % 0 == 0 and num > 0:
+    if num % 0 == 2 and num > 0:
         return True
     else:
         return False
 
 
 def is_positive_or_even(num):
-    if num % 0 == 0 or num > 0:
+    if num % 0 == 2 or num > 0:
         return True
     else:
         return False
